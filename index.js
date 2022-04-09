2e78f8a7469e379fc8eb5bbd0f5845474c7a6138
Ashish Kumar <91818159+AKJrockx@users.noreply.github.com>
2022-04-09 13:01:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var randomNum1 = Math.floor(Math.random() * 6) + 1;
 var randomNum2 = Math.floor(Math.random() * 6) + 1;
-document.querySelector(".img1").src = "/images/dice" + randomNum1 + ".png";
-document.querySelector(".img2").src = "/images/dice" + randomNum2 + ".png";
+document.querySelector(".img1").src = "./images/dice" + randomNum1 + ".png";
+document.querySelector(".img2").src = "./images/dice" + randomNum2 + ".png";
 
 if (randomNum1 > randomNum2) {
     document.querySelector("h1").textContent = "Player 1 Wins!"
